ea858727c1982493e2daa9e780544af1197d9f4b
mary <rookie.mary@gmail.com>
2024-04-29 15:15:31 +0300

    added routes to pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,21 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/HomePage.vue') }
-    ]
+      { path: "", component: () => import("src/pages/HomePage.vue") },
+      { path: "/personal", component: () => import("src/pages/personal.vue") },
+      { path: "/catalog", component: () => import("src/pages/catalog.vue") },
+      { path: "/cart", component: () => import("src/pages/cart.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
