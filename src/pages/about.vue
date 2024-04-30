65e679aa4f0c9fd0c8f20f28a1b7f8701f6ee22a
Daniil Lysykh <lysykh_daniil@mail.ru>
2024-04-30 22:44:53 +0300

    update personal and style

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,14 +1,24 @@
 <template>
-    <q-page style="margin: 0 auto; width: 1200px;">
+    <q-page class="wrapper">
         <p class="text-h4 text-center text-dark text-weight-bolder">О нас</p>
         <div class="flex flex-center q-mb-xl">
-            <p style="width: 800px; " class="text-h6">
+            <p  class="text-h6 format-text">
                 Мы — бесплатный сервис по поиску книг.
                 После короткого опроса наш виртуальный помощник подберёт литературу на ваш вкус с учётом оценок других пользователей и новинок. Потратьте своё время с удовольствием от чтения. 
             </p>
-            <p style="width: 800px;" class="text-h6">
+            <p class="text-h6 format-text">
                 На наших полках книги всех значимых издательств: «Эксмо», «АСТ», «Манн, Иванов и Фербер», «Азбука», «Альпина», «Синдбад» и другие. А среди издательских брендов, которые представлены в Shook, есть: «БОМБОРА», «Corpus», «Mainstream», «Редакция Елены Шубиной», «Вилли-Винки», «Fanzon», «Комильфо» и «Канц-Эксмо».
             </p>
         </div>
     </q-page>
 </template>
+
+<style>
+    .wrapper {
+        margin: 0 auto; 
+        width: 1200px;
+    }
+    .format-text{
+        width: 800px;
+    }
+</style>
--- src/pages/personal.vue
+++ src/pages/personal.vue
@@ -1,3 +1,40 @@
 <template>
-  <h2>Личный кабинет</h2>
+  <q-page class="wrapper">
+    <div class="flex flex">
+      <p class="text-h4 text-weight-bold text-dark q-mt-xl q-mb-xl q-mr-xl">Личный кабинет</p>
+      <p class="text-h4 text-weight-bold text-dark q-mt-xl q-mb-xl q-ml-xl">Мои заказы</p>
+    </div>
+      <div class="flex">
+        <div class="shadow-4 q-pa-md q-mr-xl rounded-borders box-personal">
+        <q-avatar size="100px" font-size="52px" color="teal" text-color="white" class="q-mb-">
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>  
+        <p class="text-h6 text-bold">Имя Фамилия</p>
+        <p class="text-h6 text-bold">mail</p>
+      </div>
+
+      <div class="shadow-4 q-pa-md q-ml-xl box-order rounded-borders">
+          <div class="flex justify-between">
+            <p class="text-h6 text-bold">Номер</p>
+            <p class="text-h6 text-bold">Заказ</p>
+            <p class="text-h6 text-bold">Дата</p>
+            <p class="text-h6 text-bold">Сумма</p> 
+          </div>
+      </div>
+    </div>
+    
+  </q-page>
 </template>
+
+<style>
+  .box-order {
+    width: 600px;
+  }
+  .wrapper {
+    margin: 0 auto; 
+    width: 1000px;
+  }
+  .box-personal {
+    width: 300px;
+  }  
+</style>
