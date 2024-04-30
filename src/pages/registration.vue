adb301ee47e87a056ec91f79cd876b6a943cf073
Sergey <blobziktv@gmail.com>
2024-04-30 21:08:05 +0300

    add registration

---
+++ src/pages/registration.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-if="loggedIn">
+      <button @click="logout">LogOut</button>
+      <h1 style="color: purple">Hello from there</h1>
+      <h2>name: {{ user.name }}</h2>
+      <h2>email: {{ user.email }}</h2>
+      <img :src="user.picture" />
+    </div>
+    <div v-else>
+      <GoogleLogin :callback="callback" prompt auto-login />
+    </div>
+  </div>
+</template>
+
+<script>
+import { decodeCredential, googleLogout } from "vue3-google-login";
+export default {
+  data() {
+    return {
+      loggedIn: false,
+      user: null,
+
+      callback: (response) => {
+        console.log("LoggedIn");
+        this.loggedIn = true;
+        console.log(response);
+        this.user = decodeCredential(response.credential);
+      },
+    };
+  },
+  methods: {
+    logout() {
+      googleLogout();
+      this.loggedIn = false;
+    },
+  },
+};
+</script>
