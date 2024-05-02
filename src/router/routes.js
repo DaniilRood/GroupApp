eb9d15689e6d8d152407c107bd53f7d3b42014ca
mary <rookie.mary@gmail.com>
2024-05-02 18:48:50 +0300

    Created new component 'CatalogItem', Added items to HomePage, Overall Catalog enhancing

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,19 +5,18 @@ const routes = [
     children: [
       { path: "", component: () => import("src/pages/HomePage.vue") },
       { path: "/personal", component: () => import("src/pages/personal.vue") },
-      { path: "/catalog", component: () => import("src/pages/catalog.vue") },
+      { path: "/catalog", component: () => import("src/pages/Catalog.vue") },
       { path: "/cart", component: () => import("src/pages/cart.vue") },
-      { path: "/chat", component: () => import("src/pages/chat.vue") },
+      { path: "/chat", component: () => import("src/pages/Chat.vue") },
       { path: "/about", component: () => import("src/pages/about.vue") },
-      { path: "/auth", component: () => import("src/pages/auth.vue") }
+      { path: "/auth", component: () => import("src/pages/auth.vue") },
     ],
   },
-
 
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
   },
 ];
-  
+
 export default routes;
