d9684a3d4cd41d9b3c4a8265646dd454c5c56f76
Daniil Lysykh <lysykh_daniil@mail.ru>
2024-05-04 11:03:38 +0300

    .

--- src/pages/auth.vue
+++ src/pages/auth.vue
@@ -1,66 +1,66 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card style="width: 400px">
-      <q-card-section>
-        <div class="text-h6">{{ isLogin ? "Вход" : "Регистрация" }}</div>
-      </q-card-section>
-
-      <q-form @submit.prevent="isLogin ? login() : register()" class="q-mb-xl">
-        <q-card-section v-if="!isLogin">
-          <q-input
-            filled
-            v-model="name"
-            label="Имя"
-            lazy-rules
-            class="q-mb-md"
-          />
-          <q-input
-            filled
-            v-model="surname"
-            label="Фамилия"
-            lazy-rules
-            class="q-mb-md"
-          />
-        </q-card-section>
-
+    <q-page class="flex flex-center">
+        <q-card style="width: 400px">
         <q-card-section>
-          <q-input
-            filled
-            v-model="email"
-            label="E-mail"
-            lazy-rules
-            class="q-mb-md"
-          />
-          <q-input
-            filled
-            v-model="password"
-            label="Пароль"
-            type="password"
-            lazy-rules
-            class="q-mb-md"
-          />
+            <div class="text-h6">{{ isLogin ? "Вход" : "Регистрация" }}</div>
         </q-card-section>
+
+        <q-form @submit.prevent="isLogin ? login() : register()" class="q-mb-xl">
+            <q-card-section v-if="!isLogin">
+            <q-input
+                filled
+                v-model="name"
+                label="Имя"
+                lazy-rules
+                class="q-mb-md"
+            />
+            <q-input
+                filled
+                v-model="surname"
+                label="Фамилия"
+                lazy-rules
+                class="q-mb-md"
+            />
+            </q-card-section>
+
+            <q-card-section>
+            <q-input
+                filled
+                v-model="email"
+                label="E-mail"
+                lazy-rules
+                class="q-mb-md"
+            />
+            <q-input
+                filled
+                v-model="password"
+                label="Пароль"
+                type="password"
+                lazy-rules
+                class="q-mb-md"
+            />
+            </q-card-section>
+
+            <q-card-actions align="center">
+            <q-btn
+                :label="isLogin ? 'Войти' : 'Зарегистрироваться'"
+                type="submit"
+                color="primary"
+            />
+            </q-card-actions>
+        </q-form>
 
         <q-card-actions align="center">
-          <q-btn
-            :label="isLogin ? 'Войти' : 'Зарегистрироваться'"
-            type="submit"
-            color="primary"
-          />
+            <q-btn flat no-caps @click="isLogin = !isLogin">
+            {{
+                isLogin
+                ? "Нет аккаунта? Зарегистрироваться"
+                : "Уже есть аккаунт? Войти"
+            }}
+            </q-btn>
         </q-card-actions>
-      </q-form>
-
-      <q-card-actions align="center">
-        <q-btn flat no-caps @click="isLogin = !isLogin">
-          {{
-            isLogin
-              ? "Нет аккаунта? Зарегистрироваться"
-              : "Уже есть аккаунт? Войти"
-          }}
-        </q-btn>
-      </q-card-actions>
-    </q-card>
-  </q-page>
+        </q-card>
+    </q-page>
 </template>
 
 <script setup>
@@ -69,9 +69,12 @@ import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
 import { auth } from "../firebase";
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 } from "firebase/auth";
+import { useStore } from '../stores/user'
+
+const store = useStore();
 
 const isLogin = ref(true);
 const name = ref("");
@@ -82,24 +85,37 @@ const $q = useQuasar();
 const router = useRouter();
 
 const register = async () => {
-  try {
-    await createUserWithEmailAndPassword(auth, email.value, password.value);
-    localStorage.setItem("name", name.value);
-    localStorage.setItem("surname", surname.value);
-    router.push("/");
-  } catch (error) {
-    console.error("Ошибка регистрации: ", error);
-  }
+try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+    const user = userCredential.user;
+    await user.updateProfile({
+        displayName: `${name.value} ${surname.value}`,
+    });
+    store.setUser({
+        name: name.value,
+        surname: surname.value,
+        email: email.value,
+        id: user.uid,
+    });
+    router.push('/');
+    } catch (error) {
+    console.error('Ошибка регистрации: ', error);
+    }
 };
 
 const login = async () => {
-  try {
-    await signInWithEmailAndPassword(auth, email.value, password.value);
-    localStorage.setItem("name", name.value);
-    localStorage.setItem("surname", surname.value);
-    router.push("/");
-  } catch (error) {
-    console.error("Ошибка входа: ", error);
-  }
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+        const user = userCredential.user;
+    store.setUser({
+        name: user.displayName.split(' ')[0],
+        surname: user.displayName.split(' ')[1],
+        email: user.email,
+        id: user.uid,
+    });
+    router.push('/personal');
+    } catch (error) {
+    console.error('Ошибка входа: ', error);
+    }
 };
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,12 +4,12 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [
       { path: "", component: () => import("src/pages/HomePage.vue") },
-      { path: "/personal", component: () => import("src/pages/personal.vue") },
+      { path: "/personal", component: () => import("src/pages/Personal.vue") },
       { path: "/catalog", component: () => import("src/pages/Catalog.vue") },
-      { path: "/cart", component: () => import("src/pages/cart.vue") },
+      { path: "/cart", component: () => import("src/pages/Cart.vue") },
       { path: "/chat", component: () => import("src/pages/Chat.vue") },
-      { path: "/about", component: () => import("src/pages/about.vue") },
-      { path: "/auth", component: () => import("src/pages/auth.vue") },
+      { path: "/about", component: () => import("src/pages/About.vue") },
+      { path: "/auth", component: () => import("src/pages/Auth.vue") },
     ],
   },
 
