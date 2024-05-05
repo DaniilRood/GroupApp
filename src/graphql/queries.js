c750a8aaf8433f5c34a514a191c13bddeb7ff956
mary <rookie.mary@gmail.com>
2024-05-05 17:54:06 +0300

    new functionality in Catalog: sorting and filtering

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -13,3 +13,65 @@ export const getBooks = gql`
     }
   }
 `;
+
+export const getBooksByDescPrice = gql`
+  query BooksCatalog {
+    books(order_by: { price: desc }) {
+      id
+      image
+      title
+      author
+      price
+      genre
+      new
+    }
+  }
+`;
+
+export const getBooksByAscPrice = gql`
+  query BooksCatalog {
+    books(order_by: { price: asc }) {
+      id
+      image
+      title
+      author
+      price
+      genre
+      new
+    }
+  }
+`;
+
+export const getBooksNew = gql`
+  query BooksCatalog {
+    books(order_by: { created_at: desc }) {
+      id
+      image
+      title
+      author
+      price
+      genre
+      new
+    }
+  }
+`;
+
+export const getBooksByGenre = (genres) => {
+  let finalQueryString = "";
+  genres.forEach((genre) => {
+    finalQueryString += `{genre: {_eq: "${genre}"}},`;
+  });
+  return gql`
+  query BooksCatalog {
+    books(where: {_or: [${finalQueryString}]}) {
+      id
+      image
+      title
+      author
+      price
+      genre
+      new
+    }
+  }
+  `;
+};
