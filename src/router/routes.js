906ccf77dd643a35ea6d1a25f9065fb4ab4de3eb
Daniil Lysykh <lysykh_daniil@mail.ru>
2024-05-08 12:28:03 +0300

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,8 +8,14 @@ const routes = [
       { path: "/catalog", component: () => import("src/pages/Catalog.vue") },
       { path: "/cart", component: () => import("src/pages/Cart.vue") },
       { path: "/chat", component: () => import("src/pages/Chat.vue") },
+<<<<<<< HEAD
       { path: "/about", component: () => import("src/pages/About.vue") },
       { path: "/auth", component: () => import("src/pages/Auth.vue") },
+=======
+      { path: "/about", component: () => import("src/pages/about.vue") },
+      { path: "/signup", component: () => import("src/pages/SignUp.vue") },
+      { path: "/signin", component: () => import("src/pages/Signin.vue") },
+>>>>>>> 833bceb (update)
     ],
   },
 
